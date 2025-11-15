e7d282e2e6c96444fe3143f677451431a2973619
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2025-11-15 11:09:01 -0500

    add keyboard direction handling with movement restrictions

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Direction } from './enum/direction';
 
 @Component({
   selector: 'app-board',
@@ -9,15 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  direction: Direction = Direction.Right;
+
   gridSize: number = 10;
   cells: number[] = [];
   snake: number[] = [];
   food = 0;
 
+
   ngOnInit(): void {
     this.initBoard();
     this.placeInitialSnake();
     this.placeFood();
+    window.addEventListener('keydown', this.handleKeyEvent);
+    this.startGameLoop();
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('keydown', this.handleKeyEvent);
+  }
+
+  startGameLoop() {
+    setInterval(() => {
+      this.moveSnake();
+    }, 150);
   }
 
   private initBoard() {
@@ -62,6 +78,22 @@ export class BoardComponent implements OnInit {
   }
   isSnakeTail(index: number): boolean {
     return this.snake.length > 0 && index === this.snake[this.snake.length - 1];
+  }
+
+  private handleDirection(key: string) {
+    if (key === 'ArrowUp' && this.direction !== Direction.Down) {
+      this.direction = Direction.Up;
+    } else if (key === 'ArrowDown' && this.direction !== Direction.Up) {
+      this.direction = Direction.Down;
+    } else if (key === 'ArrowLeft' && this.direction !== Direction.Right) {
+      this.direction = Direction.Left;
+    } else if (key === 'ArrowRight' && this.direction !== Direction.Left) {
+      this.direction = Direction.Right;
+    }
+  }
+
+  private handleKeyEvent = (event: KeyboardEvent) => {
+    this.handleDirection(event.key);
   }
   
 }
