07b4430f6771891a33508f131d1a458c29a1b7be
Ricardo Pacheco <ricardo.pachecoescobar@gmail.com>
2026-01-18 15:53:24 -0500

    refactor controls component

--- src/app/components/controls/controls.component.ts
+++ src/app/components/controls/controls.component.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { GameSnapshot } from '../../services/models/game-snapshot';
+import { GameState } from '../board/enum/game-state';
+import { Direction } from '../board/enum/direction';
+import { GameService } from '../../services/game.service';
 import { AudioService } from '../../services/audio.service';
 
 @Component({
@@ -9,32 +14,76 @@ import { AudioService } from '../../services/audio.service';
   styleUrl: './controls.component.scss'
 })
 export class ControlsComponent {
+  private sub!: Subscription;
 
-  @Output() direction = new EventEmitter<string>();
-  @Output() pauseClicked = new EventEmitter<void>();
+  snapshot!: GameSnapshot;
+  gameState = GameState;
+  direction = Direction;
 
-  @Input() isPaused: boolean = false;
-  @Input() isGameOver: boolean = false;
+  isMuted: boolean = false;
   
   constructor(
-    private audioService: AudioService) 
-  { }
+    private gameService: GameService,
+    private audioService: AudioService,
+  ) {}
 
-  emitDirection(dir: string) {
-    this.direction.emit(dir);
+  ngOnInit(): void {
+    this.sub = this.gameService.snapshot$.subscribe(s => {
+      this.snapshot = s;
+    })
   }
 
-  emitPause() {
-    this.pauseClicked.emit();
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
-  get isMuted(): boolean {
-    return this.audioService.isMuted();
+  /* ======================
+     Actions
+     ====================== */
+  
+  move(dir: Direction) {
+    this.gameService.setDirection(dir);
   }
 
-  toggleMute() {
-    if (this.isGameOver) return;
+  togglePlayPause(): void {
+    if (!this.snapshot) return;
+
+    switch (this.snapshot.state) {
+      case this.gameState.Start:
+        this.audioService.unlock();
+        this.audioService.startMusic();
+        this.gameService.start();
+        break;
+      case this.gameState.Playing:
+        this.audioService.pauseMusic();
+        this.gameService.pause();
+        break;
+      case this.gameState.Paused:
+        this.audioService.resumeMusic();
+        this.gameService.resume();
+        break;
+    }
+  }
+  
+  toggleMute(): void {
     this.audioService.toggleMute();
   }
 
+  restart(): void {
+    this.audioService.stopMusic();
+    this.gameService.reset();
+  }
+
+
+  /* ======================
+    UI helpers
+    ====================== */
+
+  get isPaused(): boolean {
+    return this.snapshot.state === this.gameState.Paused || this.snapshot.state === this.gameState.Start;
+  }
+
+  get isGameOver(): boolean {
+    return this.snapshot.state === this.gameState.GameOver;
+  }
 }
